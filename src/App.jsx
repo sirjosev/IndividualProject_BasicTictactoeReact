5d9a8e9b4a2800c35a9ae3d4615acdb59f75568f
Sir Joseph <sirjosephholmes@gmail.com>
2024-03-30 16:17:49 +0700

    erorr bikin handling click nya

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react'
 
-function Kotak() {
+function Kotak({nilai, onKotakClick}) {
+
   return (
-    <button className='kotak'>X</button>
+    <button className='kotak' onClick={onKotakClick}>{nilai}</button>
   )
 }
 
-function App() {
+export default function Papan() {
+  const [kotaks, setKotaks] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextKotak= kotaks.slice();
+    nextKotak[i] = 'X';
+    setKotaks(nextKotak); 
+  }
 
   return (
     <div className='board'>
-      <Kotak /> 
-      <Kotak /> 
-      <Kotak /> 
-      <Kotak /> 
-      <Kotak /> 
-      <Kotak /> 
-      <Kotak /> 
-      <Kotak /> 
-      <Kotak /> 
+      <Kotak value={kotaks[0]} onKotakClick={() => handleClick(0)}/> 
+      <Kotak value={kotaks[1]}/> 
+      <Kotak value={kotaks[2]}/> 
+      <Kotak value={kotaks[3]}/> 
+      <Kotak value={kotaks[4]}/> 
+      <Kotak value={kotaks[5]}/> 
+      <Kotak value={kotaks[6]}/> 
+      <Kotak value={kotaks[7]}/> 
+      <Kotak value={kotaks[8]}/> 
     </div>
   )
 }
 
-export default App
